15995e4d6002fee488d0c18f66e3defae8686c71
Benton Liu <bentonliu@bentonliu.com>
2018-11-29 01:58:51 -0800

    fixing files

--- src/main/java/com/mybetterplanner/springbootwebapplicationbootstrapjquery/SpringBootWebApplicationBootstrapJqueryApplication.java
+++ src/main/java/com/mybetterplanner/springbootwebapplicationbootstrapjquery/SpringBootWebApplicationBootstrapJqueryApplication.java
@@ -9,26 +9,6 @@ import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class SpringBootWebApplicationBootstrapJqueryApplication {
-
-	@Bean
-	public CorsFilter corsFilter() {
-
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true); // you USUALLY want this
-		config.addAllowedOrigin("*");
-		config.addAllowedHeader("*");
-		config.addAllowedMethod("OPTIONS");
-		config.addAllowedMethod("HEAD");
-		config.addAllowedMethod("GET");
-		config.addAllowedMethod("PUT");
-		config.addAllowedMethod("POST");
-		config.addAllowedMethod("DELETE");
-		config.addAllowedMethod("PATCH");
-		source.registerCorsConfiguration("/**", config);
-		return new CorsFilter(source);
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWebApplicationBootstrapJqueryApplication.class, args);
 	}
