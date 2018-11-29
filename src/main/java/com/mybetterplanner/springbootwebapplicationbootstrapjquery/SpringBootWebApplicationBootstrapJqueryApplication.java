bf80c6fcf3754c49356574419b86f4348142ce99
Benton Liu <bentonliu@bentonliu.com>
2018-11-29 01:19:46 -0800

    Update SpringBootWebApplicationBootstrapJqueryApplication.java

--- src/main/java/com/mybetterplanner/springbootwebapplicationbootstrapjquery/SpringBootWebApplicationBootstrapJqueryApplication.java
+++ src/main/java/com/mybetterplanner/springbootwebapplicationbootstrapjquery/SpringBootWebApplicationBootstrapJqueryApplication.java
@@ -2,10 +2,33 @@ package com.mybetterplanner.springbootwebapplicationbootstrapjquery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class SpringBootWebApplicationBootstrapJqueryApplication {
 
+	@Bean
+	public CorsFilter corsFilter() {
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		//config.setAllowCredentials(true); // you USUALLY want this
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("OPTIONS");
+		config.addAllowedMethod("HEAD");
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("PUT");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("DELETE");
+		config.addAllowedMethod("PATCH");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWebApplicationBootstrapJqueryApplication.class, args);
 	}
