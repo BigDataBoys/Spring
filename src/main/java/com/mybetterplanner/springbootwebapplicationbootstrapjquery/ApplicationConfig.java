acc41da00b1fc977be63321424e0d70bad7a7c9b
Austin Buenaobra <ausbozz@gmail.com>
2018-11-29 01:44:45 -0800

    Revert "Merge branch 'master' of https://github.com/BigDataBoys/Spring"
    This reverts commit 81b9d3ca3d3f0319a4b9144878c6c0f970f1686c, reversing
    changes made to 95a26a77ecbef2d40e96e806bacc4880ae29a548.

--- src/main/java/com/mybetterplanner/springbootwebapplicationbootstrapjquery/ApplicationConfig.java
+++ src/main/java/com/mybetterplanner/springbootwebapplicationbootstrapjquery/ApplicationConfig.java
@@ -6,9 +6,9 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-//@Configuration
-//@ComponentScan
-//@EnableWebMvc
+@Configuration
+@ComponentScan
+@EnableWebMvc
 
 public class ApplicationConfig extends WebMvcConfigurerAdapter {
 
